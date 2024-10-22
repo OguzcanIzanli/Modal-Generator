710ea4464bedff26eeff5348edf389ee50b8a959
oguzcan izanli <oguzcanizanli@gmail.com>
2024-10-22 04:45:56 +0300

    Modal 26 added.

--- src/app/components/modal/Templates/Template17.tsx
+++ src/app/components/modal/Templates/Template17.tsx
@@ -126,9 +126,9 @@ if (typeof window !== "undefined") {
           // Create a link element to load the external Tailwind CSS file
           const linkElem = document.createElement("link");
           linkElem.rel = "stylesheet"; // Set the relation to 'stylesheet'
-          linkElem.href = "http://localhost:3000/dist/tailwind.css"; // Set the href to point to the Tailwind CSS file
-          //   linkElem.href =
-          // "https://modal-generator.netlify.app/dist/tailwind.css";
+          //   linkElem.href = "http://localhost:3000/dist/tailwind.css"; // Set the href to point to the Tailwind CSS file
+          linkElem.href =
+            "https://modal-generator.netlify.app/dist/tailwind.css";
           shadow.appendChild(linkElem); // Append the link element to the shadow DOM to load the styles
 
           // Once the CSS file is fully loaded, proceed with rendering the modal
